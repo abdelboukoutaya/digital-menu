df0221f0d6c260c71504737941a6da8ad42e6519
Abdel adem <abdeladem.b@gmail.com>
2026-01-02 00:55:03 +0100

    add start script

--- backend/server.js
+++ backend/server.js
@@ -13,3 +13,7 @@ const PORT = process.env.PORT || 3000;   // ✅ important
 app.listen(PORT, () => {
   console.log("✅ API running on port", PORT);
 });
+
+"scripts": {
+  "start": "node server.js"
+}
