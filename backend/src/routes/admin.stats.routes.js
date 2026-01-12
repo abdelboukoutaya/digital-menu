cc24c4b4db18cd8465ae78a3231a5defd799457c
Abdel adem <abdeladem.b@gmail.com>
2026-01-12 13:19:22 +0100

    fix(admin): add missing admin stats controller

--- backend/src/routes/admin.stats.routes.js
+++ backend/src/routes/admin.stats.routes.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-const { getStats } = require("../controllers/admin.stats.controller")
+const {
+    getStats
+} = require("../controllers/admin.stats.controller")
 
 router.get("/", getStats)
 
